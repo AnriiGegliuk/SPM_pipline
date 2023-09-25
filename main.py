99980f161a179c18ac1fb50cde6eef014c266c49
AnriiGegliuk <andrii.gegliuk@gmail.com>
2023-09-25 16:34:49 +0900

    file modified and comment added

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import os
 import argparse
 from utils.identify_nii import find_images
 
-
-
-
-
-
+# creating argparse arguments to run the function
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Find and count medical image files in a folder and its subfolders.')
     parser.add_argument('folder_path', type=str, help='Path to the folder to be searched')
