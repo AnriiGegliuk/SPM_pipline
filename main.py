a69bf3aa1f147e6180977ce554a2731513b5ce91
AnriiGegliuk <andrii.gegliuk@gmail.com>
2023-09-25 16:06:46 +0900

    argparse added to test identify_nii fucntion

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import argparse
+from utils.identify_nii import find_images
+
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Find and count medical image files in a folder and its subfolders.')
+    parser.add_argument('folder_path', type=str, help='Path to the folder to be searched')
+    
+    args = parser.parse_args()
+    find_images(args.folder_path)
